639138c5a910febd09b3276d046f86841f2474c2
pooh <im.haru3@gmail.com>
2024-02-04 17:13:47 +0900

    각 페이지별 폴더화 및 main.php 뼈대 작성

---
+++ main.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+</head>
+<body>
+    <div id="warp">
+        <header>
+            <h1>Logo</h1>
+        </header>
+        <main>
+            <nav>
+                <div id="nav_auth">
+
+                </div>
+                <div id="nav_board">
+
+                </div>
+            </nav>
+            <section>
+                <div id="view_board">
+
+                </div>
+            </section>
+        </main>
+        <footer>
+            <H1>Footer</H1>
+        </footer>
+    </div>
+</body>
+</html>
